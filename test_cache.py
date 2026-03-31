e97dc286e4631ea94efb5ad47153c7d431535002
tomo <1ornoln@gmail.com>
2026-03-31 17:56:15 +0900

    update

--- test_cache.py
+++ test_cache.py
@@ -1,9 +1,14 @@
+# main.py
+# 全体の流れを管理するコード
 
 from src.audio_table.run_audio_table_pipeline import main as run_audio_table_pipeline
 
 
+
 def main():
+
     run_audio_table_pipeline()
+
 
 
 if __name__ == "__main__":
